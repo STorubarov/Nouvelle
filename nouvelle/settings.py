2e33bed2ef542fbc00febbe5d91b7d3de4940cd1
STorubarov <tarkin@gmail.com>
2017-06-18 15:04:47 +0300

    Merge remote-tracking branch 'nouvelle/master'
    # Conflicts:
    #	nouvelle/settings.py

--- nouvelle/settings.py
+++ nouvelle/settings.py
@@ -107,6 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
+LANGUAGE_CODE = 'ru'
+
 LANGUAGES = (
     ('ru', 'Russian'),
     ('en', 'English'),
